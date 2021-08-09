23261b86b45b47bb6c8b42bec85f4d7317881204
donghoonlee94 <94donghoon.lee@gmail.com>
2021-08-09 22:11:47 +0900

    Add input

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>Vue Todo</h1>
+    <TodoInput :item="todoText" @input="updateTodotext" @add="addTodoItem" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoInput from "./components/TodoInput.vue";
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: "",
+    };
+  },
+  methods: {
+    updateTodotext(v: any) {
+      this.todoText = v;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      localStorage.setItem(value, value);
+      this.todoText = "";
+    },
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label for="todo-input">오늘 할 일 : </label>
+    <input type="text" id="todo-input" :value="item" @input="handleInput" />
+    <button type="button" @click="addTodo">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["item"],
+  methods: {
+    handleInput(e: any) {
+      this.$emit("input", e.target.value);
+    },
+    addTodo() {
+      this.$emit("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
